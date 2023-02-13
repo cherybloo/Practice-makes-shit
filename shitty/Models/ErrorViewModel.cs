b74d7e20fa6763d08980ac95ea8270f0e5a3037e
Laurencio Michael Susanto <laurenciomichael98@gmail.com>
2023-02-13 10:25:07 +0100

    Add project files.

---
+++ shitty/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shitty.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
